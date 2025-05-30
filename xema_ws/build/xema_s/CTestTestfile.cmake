daf65e92cb2880207ee9805c223ff682c04de4f5
kuuchan-code <kuuchanxn@gmail.com>
2025-05-31 00:44:04 +0900

    updatei

--- xema_ws/build/xema_s/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ikeda/xema_ws/src/xema_s
-# Build directory: /home/ikeda/xema_ws/build/xema_s
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
